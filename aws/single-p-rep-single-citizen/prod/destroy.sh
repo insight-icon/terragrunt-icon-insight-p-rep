4d385289a9573fe56a7a283069743472abd2ab84
robc-io <robc.io.opensource@gmail.com>
2019-09-09 23:50:47 -0700

    fixing destroy

--- aws/single-p-rep-single-citizen/prod/destroy.sh
+++ aws/single-p-rep-single-citizen/prod/destroy.sh
@@ -5,9 +5,9 @@ mkdir -p ~/.terraform.d/plugin-cache
 export TF_PLUGIN_CACHE_DIR=~/.terraform.d/plugin-cache
 
 # TODO: Turn into array + loop
-terragrunt destroy-all --terragrunt-source-update --terragrunt-non-interactive --terragrunt-working-dir citizen
+terragrunt destroy-all --terragrunt-source-update --terragrunt-non-interactive --terragrunt-working-dir us-east-1/citizen
 terragrunt destroy-all --terragrunt-source-update --terragrunt-non-interactive --terragrunt-working-dir us-east-1/p-rep
 terragrunt destroy-all --terragrunt-source-update --terragrunt-non-interactive --terragrunt-working-dir us-east-1/logging
 terragrunt destroy-all --terragrunt-source-update --terragrunt-non-interactive --terragrunt-working-dir us-east-1/network
-terragrunt destroy-all --terragrunt-source-update --terragrunt-non-interactive --terragrunt-working-dir us-east-1/global
+terragrunt destroy-all --terragrunt-source-update --terragrunt-non-interactive --terragrunt-working-dir global
 
