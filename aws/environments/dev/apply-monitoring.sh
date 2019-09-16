f484ddc70b708e46ace1f0730e061d22d6e3344e
robc-io <robc.io.opensource@gmail.com>
2019-09-16 14:25:24 -0700

    adding monitoring apply

--- aws/environments/dev/apply-monitoring.sh
+++ aws/environments/dev/apply-monitoring.sh
@@ -8,15 +8,23 @@ export TF_INPUT=0
 DIRECTORIES=( \
 "global/profiles/p-rep" \
 "global/profiles/citizen" \
+"us-east-1/network/vpc" \
+"us-east-1/network/vpc-services" \
+"us-east-1/network/peering-main-services" \
 "us-east-1/logging/log-config-bucket" \
+"us-east-1/services/eks" \
 "us-east-1/p-rep/keys" \
 "us-east-1/p-rep/sg" \
-"us-east-1/p-rep/ec2" \
-"us-east-1/network/vpc" \
+"us-east-1/p-rep/ec2-monitoring" \
 "us-east-1/citizen/keys" \
 "us-east-1/citizen/sg" \
-"us-east-1/citizen/ec2" \
+"us-east-1/citizen/ec2-monitoring" \
 )
+
+#"us-east-1/network/vpc-mgmt" \
+#"us-east-1/network/peering-mgmt" \
+
+
 
 for i in "${DIRECTORIES[@]}"
 do
--- aws/environments/dev/us-east-1/network/vpc-services/terragrunt.hcl
+++ aws/environments/dev/us-east-1/network/vpc-services/terragrunt.hcl
@@ -10,7 +10,7 @@ include {
 inputs = {
   enable_nat_gateway = "false"
 //  single_nat_gateway = "false"
-  name = "main-net-mgmt-vpc"
+  name = "services-vpc"
   enable_dns_hostnames = "true"
   enable_dns_support = "true"
 
