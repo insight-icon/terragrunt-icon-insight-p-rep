2d9be7c05e37ed588f2a1f258b12a85325131b64
robc-io <robc.io.opensource@gmail.com>
2019-09-16 14:28:42 -0700

    adding monitoring destroy

---
+++ aws/environments/dev/destroy-monitoring.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Cache the plugins
+mkdir -p ~/.terraform.d/plugin-cache
+export TF_PLUGIN_CACHE_DIR=~/.terraform.d/plugin-cache
+export TF_INPUT=0
+
+DIRECTORIES=( \
+"us-east-1/citizen/ec2-monitoring" \
+"us-east-1/p-rep/ec2-monitoring" \
+"us-east-1/services/eks" \
+"us-east-1/p-rep/keys" \
+"us-east-1/p-rep/sg" \
+"us-east-1/citizen/keys" \
+"us-east-1/citizen/sg" \
+"global/profiles/p-rep" \
+"global/profiles/citizen" \
+"us-east-1/logging/log-config-bucket" \
+"us-east-1/network/vpc-services" \
+"us-east-1/network/peering-main-services" \
+"us-east-1/network/vpc" \
+)
+
+#"us-east-1/network/vpc-mgmt" \
+#"us-east-1/network/peering-mgmt" \
+
+
+
+for i in "${DIRECTORIES[@]}"
+do
+   terragrunt apply --terragrunt-source-update --terragrunt-non-interactive --auto-approve --terragrunt-working-dir$i
+done
