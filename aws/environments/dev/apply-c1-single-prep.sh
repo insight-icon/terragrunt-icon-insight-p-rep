7c01baa5661c0a1671dab4ecbdc39707c0ddb14f
Stacy <alme0056@umn.edu>
2019-10-01 13:38:00 -0700

    running from ansible

--- aws/environments/dev/apply-c1-single-prep.sh
+++ aws/environments/dev/apply-c1-single-prep.sh
@@ -6,12 +6,12 @@ export TF_PLUGIN_CACHE_DIR=~/.terraform.d/plugin-cache
 export TF_INPUT=0
 
 DIRECTORIES=( \
-#"global/profiles/p-rep" \
-#"us-east-1/logging/log-config-bucket" \
-#"us-east-1/network/vpc" \
-#"us-east-1/p-rep/keys" \
-#"us-east-1/p-rep/sg" \
-#"us-east-1/p-rep/ec2" \
+# "global/profiles/p-rep" \
+# "us-east-1/logging/log-config-bucket" \
+# "us-east-1/network/vpc" \
+# "us-east-1/p-rep/keys" \
+# "us-east-1/p-rep/sg" \
+# "us-east-1/p-rep/ec2" \
 "us-east-1/p-rep/ec2-configuration-c1" \
 )
 
