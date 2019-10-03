548741a4d0733258afcf0bda0d566fdc246ffe2c
robc-io <robc.io.opensource@gmail.com>
2019-10-03 02:54:29 -0700

    fixing destroy

--- aws/environments/dev/destroy-c3-single-prep.sh
+++ aws/environments/dev/destroy-c3-single-prep.sh
@@ -16,7 +16,7 @@ DIRECTORIES=( \
 "us-east-1/p-rep/dns-c1" \
 "us-east-1/p-rep/ec2" \
 "us-east-1/p-rep/keys" \
-#"us-east-1/p-rep/sg" \
+"us-east-1/p-rep/sg" \
 "us-east-1/data" \
 "us-east-1/bastion/sg" \
 "us-east-1/logging/log-config-bucket" \
