6da4d68d99c4ec95d8f1a4e54cf2a60ec2a04252
Soe San Win <soe.san.win.oss@gmail.com>
2019-10-13 20:04:37 -0700

    Generic Bash Wrapper around Terragrunt Operations
    First candidate implementation of bash wrapper around terragrunt
    operations.
    
    The configuration to be applied can be supplied via json format
    configuration file, which defaults to the configs directory at the same
    level as the wrapper script.
    
    Signed-off-by: Soe San Win <soe.san.win.oss@gmail.com>

---
+++ aws/environments/dev/tgrun.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# -e : fail as soon as a command errors out 
+# -u : unset variables are treated as errors
+# -x : print each command before execution (debug tool)
+# -o pipefail : fail as soon as any command in pipeline errors out
+set -euo pipefail
+
+#############################################################################
+# Cache the plugins
+#############################################################################
+mkdir -p ~/.terraform.d/plugin-cache
+export TF_PLUGIN_CACHE_DIR=~/.terraform.d/plugin-cache
+export TF_INPUT=0
+
+#############################################################################
+# Setting variables
+#############################################################################
+CONFIG_DIR="./configs"
+VERSION=$(< "$CONFIG_DIR/VERSION")
+
+#############################################################################
+# Runtime variables
+#############################################################################
+OKRED='\033[91m'
+OKGREEN='\033[92m'
+OKORANGE='\033[93m'
+RESET='\e[0m'
+
+CONFIG=""
+OPS=""
+
+#############################################################################
+# Functions 
+#############################################################################
+
+# Arguments: None
+function check_deps() {
+	# Check terraform which is a dependency
+	command -v terraform >/dev/null 2>&1 || \
+		{ echo >&2 "Requires terraform to run, but is not installed. Aborting!"; exit 1; }
+
+	# Check terragrunt which is a dependency
+	command -v terragrunt >/dev/null 2>&1 || \
+		{ echo >&2 "Requires terragrunt to run, but is not installed. Aborting!"; exit 1; }
+
+	# Check terragrunt which is a dependency
+	command -v ansible >/dev/null 2>&1 || \
+		{ echo >&2 "Requires ansible to run, but is not installed. Aborting!"; exit 1; }
+
+	# Check jq which is a dependency
+	command -v jq >/dev/null 2>&1 || \
+		{ echo >&2 "Requires jq to run, but is not installed. Aborting!"; exit 1; }
+}
+
+# Arguments: None
+function show_usage() {
+	echo -e "Version: $VERSION"
+	echo -e "Usage:"
+	echo -e "  $0 <config> <ops>"
+	echo ""
+	echo -e "arguments:"
+	echo -e "  <config> : Deployment configuration to use"
+	echo -e "  <ops>    : 'apply' or 'destroy'"
+	echo ""
+}
+
+# Arguments: Config, Operation and Current Step
+function show_current_step() {
+	echo ""
+	echo -e "$OKRED**********************************************************$RESET"
+	echo -e "$OKRED*$RESET$OKORANGE Configuration:$RESET$OKGREEN $1 $RESET"
+	echo -e "$OKRED*$RESET$OKORANGE Operation:$RESET$OKGREEN $2 $RESET"
+	echo -e "$OKRED*$RESET$OKORANGE Step:$RESET$OKGREEN $3 $RESET"
+	echo -e "$OKRED**********************************************************$RESET"
+	echo ""
+}
+
+#############################################################################
+# Entry Point 
+#############################################################################
+
+# Check dependencies
+check_deps
+
+# If no arguments, show usage, and exit
+[[ -z "$*" ]] && show_usage && exit 1
+
+# Parse the arguments
+CONFIG="$1"
+OPS="$2"
+
+# Required arguments not present. Exit. 
+[[ "${CONFIG}" == "" ]] && show_usage && exit 1
+[[ "${OPS}" == "" ]] && show_usage && exit 1
+
+# If the config directory does not exist, abort.
+[[ ! -d "$CONFIG_DIR" ]] && { echo >&2 "Configuration directory not found. Aborting!"; exit 1; }
+
+# If the config specified does not exist, abort.
+CONFIG_PATH="$CONFIG_DIR/$CONFIG.json"
+[[ ! -f "$CONFIG_PATH" ]] && { echo >&2 "Configuration at $CONFIG_PATH not found. Aborting!"; exit 1; }
+
+# Apply the steps for configuration given for specified operation
+case "${OPS}" in
+	"apply")
+		while read -r step;
+		do 
+			show_current_step "${CONFIG}" "${OPS}" "$step"
+			terragrunt apply --terragrunt-source-update \
+				--terragrunt-non-interactive \
+				--auto-approve \
+				--terragrunt-working-dir "$step"
+		done < <(jq -rc ".operations.${OPS}[]" "$CONFIG_PATH")
+	;;
+	"destroy")
+		while read -r step;
+		do 
+			show_current_step "${CONFIG}" "${OPS}" "$step"
+			terragrunt destroy --terragrunt-source-update \
+				--terragrunt-non-interactive \
+				--auto-approve \
+				--terragrunt-working-dir "$step"
+		done < <(jq -rc ".operations.${OPS}[]" "$CONFIG_PATH")
+	;;
+	*)
+		echo "Unknown operation. Aborting!"
+		exit 1
+	;;
+esac
